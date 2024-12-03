57a19278945c005336c9224380554558b77c9f29
Guilherme Ramalho <137944713+GuilhermePereiraRamalho@users.noreply.github.com>
2024-12-03 09:55:06 +0100

    Prtática: Refatorando Update

--- 05sqla_async/update_main.py
+++ 05sqla_async/update_main.py
@@ -1,11 +1,15 @@
 from conf.db_session import create_session
 from models.sabor import Sabor
 from models.picole import Picole
+from sqlalchemy.future import select
+import asyncio
 
 
-def select_filtro_picole(id_picole: int) -> None:
-    with create_session() as session:
-        picole: Picole = session.query(Picole).filter(Picole.id == id_picole).one_or_none()
+async def select_filtro_picole(id_picole: int) -> None:
+    async with create_session() as session:
+        query = select(Picole).filter(Picole.id == id_picole)
+        result = await session.execute(query)
+        picole: Picole = result.unique().scalar_one_or_none()
 
         if picole:
             print(f"ID: {picole.id}")
@@ -15,43 +19,54 @@ def select_filtro_picole(id_picole: int) -> None:
             print('Não existe picole com o ID informado')
 
 
-def atualizar_sabor(id_sabor: int, novo_nome:str) -> None:
-    with create_session() as session:
-        sabor: Sabor = session.query(Sabor).filter(Sabor.id == id_sabor).one_or_none()
+async def atualizar_sabor(id_sabor: int, novo_nome:str) -> None:
+    async with create_session() as session:
+        query = select(Sabor).filter(Sabor.id == id_sabor)
+        result = await session.execute(query)
+        sabor: Sabor = result.scalar_one_or_none()
 
         if sabor:
             sabor.nome = novo_nome
-            session.commit()
+            await session.commit()
         else:
             print(f'Não existe um sabor com ID: {id_sabor}')
 
 
-def atualizar_picole(id_picole: int, novo_preco: float, novo_sabor: int = None) -> None:
-    with create_session() as session:
-        picole: Picole = session.query(Picole).filter(Picole.id == id_picole).one_or_none()
+async def atualizar_picole(id_picole: int, novo_preco: float, novo_sabor: int = None) -> None:
+    async with create_session() as session:
+        query = select(Picole).filter(Picole.id == id_picole)
+        result = await session.execute(query)
+        picole: Picole = result.unique().scalar_one_or_none()
 
         if picole:
             picole.preco = novo_preco
             if novo_sabor:
                 picole.id_sabor=novo_sabor
-            session.commit()
+            await session.commit()
         else:
             print(f'Não existe picolé com o ID: {id_picole}')
 
 
-if __name__ == '__main__':
-    # from select_main import select_filtro_sabor
-    # id_sabor = 43
-    # select_filtro_sabor(id_sabor)
+async def atualiza_sabor():
+    from select_main import select_filtro_sabor
+    id_sabor = 42
 
-    # atualizar_sabor(id_sabor=id_sabor, novo_nome="Laranja")
+    await select_filtro_sabor(id_sabor)
+    await atualizar_sabor(id_sabor=id_sabor, novo_nome="Abacate")
+    await select_filtro_sabor(id_sabor)
 
-    # select_filtro_sabor(id_sabor)
+
+async def atualiza_picole():
 
     id_picole = 21
-    novo_preco = 9.99
-    id_novo_sabor = 43
+    novo_preco = 1.99
+    id_novo_sabor = 42
 
-    select_filtro_picole(id_picole=id_picole)
-    atualizar_picole(id_picole=id_picole, novo_preco=novo_preco, novo_sabor=id_novo_sabor)
-    select_filtro_picole(id_picole=id_picole)
+    await select_filtro_picole(id_picole=id_picole)
+    await atualizar_picole(id_picole=id_picole, novo_preco=novo_preco, novo_sabor=id_novo_sabor)
+    await select_filtro_picole(id_picole=id_picole)
+
+
+if __name__ == '__main__':
+    # asyncio.run(atualiza_sabor())
+    asyncio.run(atualiza_picole())
