0a5a3c49ddcc554b4fdd5df4c1b0230a716ab582
Guilherme Ramalho <137944713+GuilhermePereiraRamalho@users.noreply.github.com>
2024-12-16 10:05:42 +0100

    Update select_main.py

--- 06sqlm_sync/select_main.py
+++ 06sqlm_sync/select_main.py
@@ -1,5 +1,6 @@
 from typing import List
 from sqlalchemy import func
+from sqlmodel import select
 from conf.helpers import formata_data
 from conf.db_session import create_session
 # select simples
@@ -13,11 +14,9 @@ from models.picole import Picole
 ## Select simples -> SELECT * FROM aditivos_nutritivos
 def select_todos_aditivos_nutritivos() -> None:
     with create_session() as session:
-        # forma 1
-        #aditivos_nutritivos: List[AditivoNutritivo] = session.query(AditivoNutritivo)
-
-        #forma 2
-        aditivos_nutritivos: List[AditivoNutritivo] = session.query(AditivoNutritivo).all()
+        query = select(AditivoNutritivo)
+        result = session.exec(query)
+        aditivos_nutritivos: List[AditivoNutritivo] = result.all()
 
         for an in aditivos_nutritivos:
             print(f'ID: {an.id}')
@@ -28,17 +27,11 @@ def select_todos_aditivos_nutritivos() -> None:
 
 def select_filtro_sabor(id_sabor: int) -> None:
     with create_session() as session:
-        # forma 1 -> Retona none caso nao encontre
-        # sabor: Sabor = session.query(Sabor).filter(Sabor.id == id_sabor).first()
+        # query = select(Sabor).filter(Sabor.id == id_sabor)
+        # result = session.exec(query)
+        # sabor: Sabor = result.one_or_none()
 
-        # forma 2 -> Retona none caso nao encontre(Recomendado)
-        # sabor: Sabor = session.query(Sabor).filter(Sabor.id == id_sabor).one_or_none()
-
-        # forma 3 -> Retona exec.NoresultFound caso nao encontr
-        # sabor: Sabor = session.query(Sabor).filter(Sabor.id == id_sabor).one()
-
-        # forma 4 -> Usando where (one, one_or_none, first)
-        sabor: Sabor = session.query(Sabor).where(Sabor.id == id_sabor).one_or_none()
+        sabor: Sabor = session.get(Sabor, id_sabor)
 
         print(f"ID: {sabor.id}")
         print(f"Data: {formata_data(sabor.data_criacao)}")
@@ -47,7 +40,9 @@ def select_filtro_sabor(id_sabor: int) -> None:
 
 def select_complexo_picole() -> None:
     with create_session() as session:
-        picoles: List[Picole] = session.query(Picole).all()
+        query = select(Picole)
+        result = session.exec(query)
+        picoles: List[Picole] = result.unique().all()
 
         for picole in picoles:
             print(f'ID: {picole.id}')
@@ -70,7 +65,9 @@ def select_complexo_picole() -> None:
 
 def select_order_by_sabor() -> None:
     with create_session() as session:
-        sabores: List[Sabor] = session.query(Sabor).order_by(Sabor.data_criacao.desc()).all()
+        query = select(Sabor).order_by(Sabor.data_criacao.desc())
+        result = session.exec(query)
+        sabores: List[Sabor] = result.all()
 
         for sabor in sabores:
             print(f"ID: {sabor.id}")
@@ -125,9 +122,9 @@ def select_agregacao() -> None:
 
 if __name__ == '__main__':
     # select_todos_aditivos_nutritivos()
-    select_filtro_sabor(21)
+    # select_filtro_sabor(21)
     # select_complexo_picole()
-    # select_order_by_sabor()
+    select_order_by_sabor()
     # select_group_by_picole()
     # select_limit()
     # select_count_revendedor()
