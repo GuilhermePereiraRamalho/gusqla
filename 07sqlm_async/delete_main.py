7231971a2db01acdd9a35e5f638b5dbae4c33810
Guilherme Ramalho <137944713+GuilhermePereiraRamalho@users.noreply.github.com>
2024-12-23 10:06:48 +0100

    Pratica: Refatorando Delete

--- 07sqlm_async/delete_main.py
+++ 07sqlm_async/delete_main.py
@@ -2,23 +2,23 @@ from typing import Optional
 from conf.db_session import create_session
 from models.revendedor import Revendedor
 from models.picole import Picole
+import asyncio
 
 
-
-def deletar_picole(id_picole: int) -> None:
-    with create_session() as session:
-        picole: Optional[Picole] = session.get(Picole, id_picole)
+async def deletar_picole(id_picole: int) -> None:
+    async with create_session() as session:
+        picole: Optional[Picole] = await session.get(Picole, id_picole)
 
         if picole:
-            session.delete(picole)
-            session.commit()
+            await session.delete(picole)
+            await session.commit()
         else:
             print(f"Picolé de id {id_picole} não encontrado!")
 
 
-def select_filtro_revendedor(id_revendedor: int) -> None:
-    with create_session() as session:
-        revendedor: Optional[Revendedor] = session.get(Revendedor, id_revendedor)
+async def select_filtro_revendedor(id_revendedor: int) -> None:
+    async with create_session() as session:
+        revendedor: Optional[Revendedor] = await session.get(Revendedor, id_revendedor)
 
         if revendedor:
             print(f'ID: {revendedor.id}')
@@ -27,42 +27,50 @@ def select_filtro_revendedor(id_revendedor: int) -> None:
             print(f'Não encontrei nenhum revendedor com id {id_revendedor}')
 
 
-def deletar_revendedor(id_revendedor: int) -> None:
-    with create_session() as session:
-        revendedor: Optional[Revendedor] = session.get(Revendedor, id_revendedor)
+async def deletar_revendedor(id_revendedor: int) -> None:
+    async with create_session() as session:
+        revendedor: Optional[Revendedor] = await session.get(Revendedor, id_revendedor)
 
         if revendedor:
-            session.delete(revendedor)
-            session.commit()
+            await session.delete(revendedor)
+            await session.commit()
         else:
             print(f'Não encontrei nenhum revendedor com id {id_revendedor}')
 
 
-if __name__ == '__main__':
-    # from update_main import select_filtro_picole
 
-    # id_picole = 21
+async def deleta_picole():
+    from update_main import select_filtro_picole
 
-    # # Antes
-    # select_filtro_picole(id_picole=id_picole)    
-
-    # # Deletar
-    # deletar_picole(id_picole=id_picole)
-
-    # # Depois
-    # select_filtro_picole(id_picole=id_picole)
-
-    # # Nao vinculado
-    # id_revendedor_nv = 3
-
-    # Vinculado
-    id_revendedor_v = 5
+    id_picole = 21
 
     # Antes
-    select_filtro_revendedor(id_revendedor=id_revendedor_v)
+    await select_filtro_picole(id_picole=id_picole)    
 
     # Deletar
-    deletar_revendedor(id_revendedor=id_revendedor_v)
+    await deletar_picole(id_picole=id_picole)
 
     # Depois
-    select_filtro_revendedor(id_revendedor=id_revendedor_v)
+    await select_filtro_picole(id_picole=id_picole)
+
+
+async def deleta_revendedor():   
+    # # Nao vinculado
+    # id_revendedor_nv = 1
+
+    # Vinculado
+    id_revendedor_v = 2
+
+    # Antes
+    await select_filtro_revendedor(id_revendedor=id_revendedor_v)
+
+    # Deletar
+    await deletar_revendedor(id_revendedor=id_revendedor_v)
+
+    # Depois
+    await select_filtro_revendedor(id_revendedor=id_revendedor_v)
+
+
+if __name__ == '__main__':
+    # asyncio.run(deleta_picole())
+    asyncio.run(deleta_revendedor())
